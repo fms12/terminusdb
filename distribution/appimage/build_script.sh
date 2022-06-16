f5cd59389be06f559b1456232aae9f5bf99eeaac
Robin de Rooij <rderooij685@gmail.com>
2022-06-16 18:50:16 +0200

    Try not to copy libedit libpcre and libbsd

--- distribution/appimage/build_script.sh
+++ distribution/appimage/build_script.sh
@@ -7,7 +7,7 @@ TERMINUSDB_DIR="${CURRENT_DIR}/app_dir/usr/share/terminusdb"
 
 function cleanup () {
     # We don't need the Rust directory at all anymore
-    # since the library is compiled in the executable
+    # since the library is copied to src/rust
     rm -rf "$TERMINUSDB_DIR"/src/rust/target
 }
 
@@ -21,12 +21,11 @@ mkdir -p app_dir/usr/lib/x86_64-linux-gnu
 rsync -r "$GIT_ROOT_DIR"/* app_dir/usr/share/terminusdb
 cp -r terminusdb/* app_dir/usr/share/terminusdb/
 cp -r /usr/lib/swi-prolog app_dir/usr/lib/
-cp -L /usr/lib/x86_64-linux-gnu/libedit.so.2 app_dir/usr/lib/swi-prolog/lib/x86_64-linux/
-cp -L /lib/x86_64-linux-gnu/libpcre.so.3 app_dir/usr/lib/swi-prolog/lib/x86_64-linux/
-cp -L /usr/lib/x86_64-linux-gnu/libbsd.so.0 app_dir/usr/lib/swi-prolog/lib/x86_64-linux/
+#cp -L /usr/lib/x86_64-linux-gnu/libedit.so.2 app_dir/usr/lib/swi-prolog/lib/x86_64-linux/
+#cp -L /lib/x86_64-linux-gnu/libpcre.so.3 app_dir/usr/lib/swi-prolog/lib/x86_64-linux/
+#cp -L /usr/lib/x86_64-linux-gnu/libbsd.so.0 app_dir/usr/lib/swi-prolog/lib/x86_64-linux/
 rm -rf app_dir/usr/lib/swi-prolog/bin/x86_64-linux/swipl-ld
 cd $TERMINUSDB_DIR && make
 cleanup
-#linuxdeploy-x86_64.AppImage --appdir ./app_dir --executable /lib/swi-prolog/bin/x86_64-linux/swipl --library /lib/swi-prolog --library ~/.local/share/swi-prolog/pack/terminus_store_prolog/rust/target/release/libterminus_store_prolog.so -d terminusdb.desktop -i swipl.png --custom-apprun AppRun --output appimage --verbosity=0
 cd "$CURRENT_DIR"
 ./linuxdeploy-x86_64.AppImage --appdir ./app_dir --executable "$SWIPL_DIR"/bin/x86_64-linux/swipl --library "$SWIPL_DIR"/lib/x86_64-linux/libswipl.so.8 --library /lib/x86_64-linux-gnu/libpcre.so.3 -d terminusdb.desktop -i terminusdb.svg --custom-apprun AppRun --output appimage --verbosity=0
